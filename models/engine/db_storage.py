73105ad8ba6c4cab90c077065496e9b37eeb5823
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-20 11:14:31 +0200

    new commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,3 +81,7 @@ class DBStorage:
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session)
         self.__session = Session()
+
+    def close(self):
+        """Closes the current database session"""
+        self.__session.close()
