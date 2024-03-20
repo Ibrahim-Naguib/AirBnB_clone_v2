b9b5d357337c6372328680ca97b3cbc380751d32
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-20 11:05:29 +0200

    new commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
-import models
 from sqlalchemy.ext.declarative import declarative_base
+import uuid
+import models
+from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
@@ -56,10 +56,8 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = (str(type(self)).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary.keys():
