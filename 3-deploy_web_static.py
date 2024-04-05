ca3ee47331340eea2d7f195250d0200e530ef32a
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-05 15:52:43 +0000

    new commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,27 +4,32 @@ script that distributes an archive to the web servers
 """
 from fabric.api import *
 from datetime import datetime
-from os.path import exists, isdir
+import os
 
 env.hosts = ['54.236.30.138', '100.25.188.43']
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generates a .tgz archive"""
+    if not os.path.isdir('versions'):
+        os.mkdir('versions')
+
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = 'versions/web_static_{}.tgz'.format(date)
+
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        print('Packing web_static to {}'.format(file_name))
+        local('tar -cvzf {} web_static'.format(file_name))
+        file_size = os.path.getsize(file_name)
+        print('web_static packed: {} -> {}Bytes'.format(file_name, file_size))
     except Exception:
-        return None
+        file_name = None
+    return file_name
 
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if os.path.exists(archive_path) is False:
         return False
     try:
         file_n = archive_path.split("/")[-1]
@@ -38,6 +43,7 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("New version deployed!")
         return True
     except Exception:
         return False
