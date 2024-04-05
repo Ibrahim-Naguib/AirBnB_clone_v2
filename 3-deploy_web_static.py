2036219dddd353453d6dc24c18f8990ed83692e4
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-05 15:25:45 +0000

    new commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,7 +12,8 @@ env.hosts = ['54.236.30.138', '100.25.188.43']
 def do_pack():
     """Generates a .tgz archive"""
     try:
-        local("mkdir -p versions")
+        if isdir("versions") is False:
+            local("mkdir versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         file_name = 'versions/web_static_{}.tgz'.format(date)
         local('tar -cvzf {} web_static'.format(file_name))
@@ -23,7 +24,9 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers"""
-    if os.path.exists(archive_path):
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
         new_name = '/data/web_static/releases/{}/'.format(archive_path[9:-4])
         put(archive_path, '/tmp/')
         run('sudo mkdir -p {}'.format(new_name))
@@ -35,8 +38,8 @@ def do_deploy(archive_path):
         run('sudo ln -s {} /data/web_static/current'.format(new_name))
         print("New version deployed!")
         return True
-
-    return False
+    except:
+        return False
 
 
 def deploy():
