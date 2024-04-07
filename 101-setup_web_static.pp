c128174ef8114663f5d01e76194eda315f6b78f0
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-07 12:18:52 +0000

    new commit

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -26,13 +26,13 @@ file { '/data/web_static/current':
   target => '/data/web_static/releases/test/',
 }
 
-exec { 'chown -R ubuntu:ubuntu /data/':
+exec { 'sudo chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 exec { 'echo':
   path    => '/usr/bin:/bin',
-  command => 'echo "server { listen 80; listen [::]:80 default_server; add_header X-Served-By ${HOSTNAME}; root /var/www/html; index index.html index.htm index.nginx-debian.html; server_name _; location /hbnb_static { alias /data/web_static/current; index index.html index.htm; } location /redirect_me { return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4; } error_page 404 /404.html; location /404.html { root /var/www/html; internal; } }" > /etc/nginx/sites-available/default'
+  command => 'echo "server { listen 80; listen [::]:80 default_server; add_header X-Served-By ${HOSTNAME}; root /var/www/html; index index.html index.htm index.nginx-debian.html; server_name _; location /hbnb_static { alias /data/web_static/current; index index.html index.htm; } location /redirect_me { return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4; } error_page 404 /404.html; location /404.html { root /var/www/html; internal; } }" | sudo tee /etc/nginx/sites-available/default'
 }
 
 service { 'nginx':
