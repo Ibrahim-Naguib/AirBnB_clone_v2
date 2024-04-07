3699e404cc23f627cace7c05eb3b9f2b657a12bd
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-07 13:35:14 +0000

    new commit

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,22 +1,30 @@
 # script that sets up the web servers for the deployment of web_static
 package { 'nginx':
-  ensure  => installed,
-}
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-exec { 'install_nginx':
-  command => 'sudo apt-get update ; sudo apt-get -y install nginx',
-  path    => '/usr/bin:/bin',
-} -> file { '/data':
+file { '/data':
   ensure  => 'directory'
-} -> file { '/data/web_static':
+} ->
+
+file { '/data/web_static':
   ensure => 'directory'
-} -> file { '/data/web_static/releases':
+} ->
+
+file { '/data/web_static/releases':
   ensure => 'directory'
-} -> file { '/data/web_static/releases/test':
+} ->
+
+file { '/data/web_static/releases/test':
   ensure => 'directory'
-} -> file { '/data/web_static/shared':
+} ->
+
+file { '/data/web_static/shared':
   ensure => 'directory'
-} -> file { '/data/web_static/releases/test/index.html':
+} ->
+
+file { '/data/web_static/releases/test/index.html':
   ensure  => present,
   content => '<html>
   <head>
@@ -26,14 +34,22 @@ exec { 'install_nginx':
   </body>
 </html>
 ',
-} -> file { '/data/web_static/current':
+} ->
+
+file { '/data/web_static/current':
   ensure => link,
   target => '/data/web_static/releases/test/',
-} -> exec { 'sudo chown -R ubuntu:ubuntu /data/':
+} ->
+
+exec { 'sudo chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
-} -> file { '/var/www/html':
+} ->
+
+file { '/var/www/html':
   ensure => 'directory'
-} -> file { '/var/www/html/index.html':
+} ->
+
+file { '/var/www/html/index.html':
   ensure  => 'present',
   content => '<html>
   <head>
@@ -43,9 +59,13 @@ exec { 'install_nginx':
   </body>
 </html>
 ',
-} -> exec { 'echo':
+} ->
+
+exec { 'echo':
   path    => '/usr/bin:/bin',
   command => "echo 'server { listen 80; listen [::]:80 default_server; add_header X-Served-By \$::HOSTNAME; root /var/www/html; index index.html index.htm index.nginx-debian.html; server_name _; location /hbnb_static { alias /data/web_static/current; index index.html index.htm; } location /redirect_me { return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4; } error_page 404 /404.html; location /404.html { root /var/www/html; internal; } }' | sudo tee /etc/nginx/sites-available/default"
-} -> exec { 'nginx restart':
+} ->
+
+exec { 'nginx restart':
   path => '/etc/init.d/'
 }
