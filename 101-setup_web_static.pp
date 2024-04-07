10b4f0ff708e3a3efe03a54b62c12f9073f43974
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-07 13:07:16 +0000

    new commit

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -6,29 +6,17 @@ package { 'nginx':
 exec { 'install_nginx':
   command => 'sudo apt-get update ; sudo apt-get -y install nginx',
   path    => '/usr/bin:/bin',
-} ->
-
-file { '/data':
+} -> file { '/data':
   ensure  => 'directory'
-} ->
-
-file { '/data/web_static':
+} -> file { '/data/web_static':
   ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases':
+} -> file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test':
+} -> file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
-
-file { '/data/web_static/shared':
+} -> file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test/index.html':
+} -> file { '/data/web_static/releases/test/index.html':
   ensure  => present,
   content => '<html>
   <head>
@@ -38,22 +26,14 @@ file { '/data/web_static/releases/test/index.html':
   </body>
 </html>
 ',
-} ->
-
-file { '/data/web_static/current':
+} -> file { '/data/web_static/current':
   ensure => link,
   target => '/data/web_static/releases/test/',
-} ->
-
-exec { 'sudo chown -R ubuntu:ubuntu /data/':
+} -> exec { 'sudo chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
-
-file { '/var/www/html':
+} -> file { '/var/www/html':
   ensure => 'directory'
-} ->
-
-file { '/var/www/html/index.html':
+} -> file { '/var/www/html/index.html':
   ensure  => 'present',
   content => '<html>
   <head>
@@ -63,13 +43,9 @@ file { '/var/www/html/index.html':
   </body>
 </html>
 ',
-} ->
-
-exec { 'echo':
+} -> exec { 'echo':
   path    => '/usr/bin:/bin',
   command => "echo 'server { listen 80; listen [::]:80 default_server; add_header X-Served-By \$::HOSTNAME; root /var/www/html; index index.html index.htm index.nginx-debian.html; server_name _; location /hbnb_static { alias /data/web_static/current; index index.html index.htm; } location /redirect_me { return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4; } error_page 404 /404.html; location /404.html { root /var/www/html; internal; } }' | sudo tee /etc/nginx/sites-available/default"
-}
-
-service { 'nginx':
+} -> service { 'nginx':
   ensure  => running,
 }
