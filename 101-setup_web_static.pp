de67e293543613dd5c4c86131c04e7af695f469f
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-07 12:57:33 +0000

    new commit

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -6,15 +6,27 @@ package { 'nginx':
 exec { 'install_nginx':
   command => 'sudo apt-get update ; sudo apt-get -y install nginx',
   path    => '/usr/bin:/bin',
-}
+} ->
 
-file { '/data/web_static/shared/':
-  ensure => directory,
-}
+file { '/data':
+  ensure  => 'directory'
+} ->
 
-file { '/data/web_static/releases/test/':
-  ensure => directory,
-}
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => present,
@@ -26,16 +38,32 @@ file { '/data/web_static/releases/test/index.html':
   </body>
 </html>
 ',
-}
+} ->
 
 file { '/data/web_static/current':
   ensure => link,
   target => '/data/web_static/releases/test/',
-}
+} ->
 
 exec { 'sudo chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+',
+} ->
 
 exec { 'echo':
   path    => '/usr/bin:/bin',
