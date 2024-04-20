a5c0f364b0711b99416250a0f2adb5e9ae3fe21a
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-20 17:49:07 +0000

    new commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -9,9 +10,9 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template("7-states_list.html", states=states)
+    return render_template("7-states_list.html", sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
