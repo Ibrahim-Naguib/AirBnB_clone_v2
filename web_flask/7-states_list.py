f7ec4820efeda8128ecb13971d7ac0dda735231e
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-20 17:54:00 +0000

    new commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Render template with states"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html", sorted_states=sorted_states)
@@ -17,8 +18,9 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown(exc):
+    """removes the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000)
